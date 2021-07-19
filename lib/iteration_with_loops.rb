291c06fa96ad4a825e2500593a7de431b9391d94
jessica you <jessicaxytan@gmail.com>
2021-07-19 15:31:48 -0400

    commit tree 32e3cb7470c4d9191e2ed04af695fdcf89f713fc

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,22 @@
 def join_nested_strings(src)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
+  
+  # empty array to contain the string sentences
+  sentence = []
+  # row count for internal arrays within nested
+  row_index = 0
+  src.map { |nested_array|
+    nested_array.map {|element|
+      if element.is_a?(String)
+        sentence << element
+      end
+    }
+  }
+  sentence.join(" ")
+  # while row_index < src.count do
+  #   element_index = 0
+  #   src[row_index].map
+  #   end
+  # end
 end
